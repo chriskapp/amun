5e352e01c6e489e4e826b0bb69820ecd46ecced8
Christoph Kappestein <k42b3.x@gmail.com>
2012-11-05 19:06:44 +0100

    add data provider class

---
+++ library/Amun/DataProvider.php
@@ -0,0 +1,130 @@
+<?php
+/*
+ *  $Id: Exception.php 635 2012-05-01 19:46:37Z k42b3.x@googlemail.com $
+ *
+ * amun
+ * A social content managment system based on the psx framework. For
+ * the current version and informations visit <http://amun.phpsx.org>
+ *
+ * Copyright (c) 2010-2012 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of amun. amun is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * amun is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with amun. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Amun_DataProvider
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://amun.phpsx.org
+ * @category   Amun
+ * @package    Amun_DataProvider
+ * @version    $Revision: 635 $
+ */
+class Amun_DataProvider
+{
+	protected $name;
+	protected $config;
+	protected $registry;
+	protected $user;
+
+	public function __construct($name, Amun_Registry $registry, Amun_User $user)
+	{
+		$this->name     = $name;
+		$this->config   = $registry->getConfig();
+		$this->registry = $registry;
+		$this->user     = $user;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function getTable()
+	{
+		return Amun_Sql_Table_Registry::get($this->getName());
+	}
+
+	public function getHandler()
+	{
+		$class = self::getClass($this->getName(), 'Handler');
+
+		if($class !== null && $class->isSubclassOf('Amun_Data_HandlerAbstract'))
+		{
+			return $class->newInstance($this->user);
+		}
+	}
+
+	public function getForm()
+	{
+		$class = self::getClass($this->getName(), 'Form');
+
+		if($class !== null && $class->isSubclassOf('Amun_Data_FormAbstract'))
+		{
+			return $class->newInstance($this->getApiEndpoint());
+		}
+	}
+
+	public function getApiEndpoint()
+	{
+		$path = strtolower(str_replace('_', '/', $this->name));
+
+		return $this->config['psx_url'] . '/' . $this->config['psx_dispatch'] . 'api/' . $path;
+	}
+
+	public function hasRight($rightName)
+	{
+		return $this->user->hasRight($this->getRightName($rightName));
+	}
+
+	public function hasViewRight()
+	{
+		return $this->hasRight('view');
+	}
+
+	public function hasAddRight()
+	{
+		return $this->hasRight('add');
+	}
+
+	public function hasEditRight()
+	{
+		return $this->hasRight('edit');
+	}
+
+	public function hasDeleteRight()
+	{
+		return $this->hasRight('delete');
+	}
+
+	protected function getRightName($rightName)
+	{
+		return strtolower($this->getName() . '_' . $rightName);
+	}
+
+	public static function getClass($namespace, $className)
+	{
+		try
+		{
+			$class = Amun_Registry::getClassName('AmunService_' . $namespace . '_' . $className);
+
+			return new ReflectionClass($class);
+		}
+		catch(ReflectionException $e)
+		{
+			return null;
+		}
+	}
+}
