fd40e594b92199f08a4862d28234604a64785447
Christoph Kappestein <k42b3.x@gmail.com>
2013-04-08 22:52:06 +0200

    add tests

---
+++ tests/Amun/Api/PipeTest.php
@@ -0,0 +1,116 @@
+<?php
+/*
+ *  $Id: PageTest.php 743 2012-06-26 19:31:26Z k42b3.x@googlemail.com $
+ *
+ * amun
+ * A social content managment system based on the psx framework. For
+ * the current version and informations visit <http://amun.phpsx.org>
+ *
+ * Copyright (c) 2010-2012 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of amun. amun is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * amun is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with amun. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Amun\Api;
+
+use Amun\DataFactory;
+use PSX\Sql\Condition;
+use PSX\Http\GetRequest;
+use PSX\Json;
+use PSX\Url;
+
+/**
+ * Amun_Api_Service_PageTest
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://amun.phpsx.org
+ * @category   tests
+ * @version    $Revision: 743 $
+ * @backupStaticAttributes disabled
+ */
+class PipeTest extends RestTest
+{
+	protected function setUp()
+	{
+		parent::setUp();
+
+		if(!$this->hasService('org.amun-project.pipe'))
+		{
+			$this->markTestSkipped('Service pipe not installed');
+		}
+	}
+
+	public function getDataSet()
+	{
+		return $this->createMySQLXMLDataSet('tests/amun.xml');
+	}
+
+	public function getEndpoint()
+	{
+		return $this->config['psx_url'] . '/' . $this->config['psx_dispatch'] . 'api/pipe';
+	}
+
+	public function getTable()
+	{
+		return DataFactory::getTable('Pipe');
+	}
+
+	public function testGet()
+	{
+		$this->assertResultSetResponse($this->get());
+	}
+
+	public function testPost()
+	{
+		$record = $this->getTable()->getRecord();
+		$record->setPageId(1);
+		$record->setMediaId(1);
+
+		$this->assertPositiveResponse($this->post($record));
+
+		$actual = $this->table->getRow(array('pageId', 'mediaId'), new Condition(array('id', '=', 1)));
+		$expect = array_map('strval', $record->getData());
+
+		$this->assertEquals($expect, $actual);
+	}
+
+	public function testSupportedFields()
+	{
+		$url      = new Url($this->getEndpoint() . '/@supportedFields');
+		$response = $this->signedRequest('GET', $url);
+
+		$this->assertEquals(200, $response->getCode());
+
+		$fields = Json::decode($response->getBody());
+
+		$this->assertEquals(true, is_array($fields));
+		$this->assertEquals(true, is_array($fields['item']));
+	}
+
+	public function testFormCreate()
+	{
+		$url      = new Url($this->getEndpoint() . '/form?method=create');
+		$response = $this->signedRequest('GET', $url);
+
+		$this->assertEquals(200, $response->getCode());
+
+		$data = Json::decode($response->getBody());
+
+		$this->assertEquals(true, is_array($data));
+		$this->assertEquals('form', $data['class']);
+		$this->assertEquals('POST', $data['method']);
+	}
+}
+
---
+++ tests/Amun/Api/SitemapTest.php
@@ -0,0 +1,89 @@
+<?php
+/*
+ *  $Id: XrdsTest.php 637 2012-05-01 19:58:47Z k42b3.x@googlemail.com $
+ *
+ * amun
+ * A social content managment system based on the psx framework. For
+ * the current version and informations visit <http://amun.phpsx.org>
+ *
+ * Copyright (c) 2010-2012 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of amun. amun is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * amun is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with amun. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Amun\Api;
+
+use Amun\DataFactory;
+use PSX\Sql\Condition;
+use PSX\Url;
+use PSX\Http\GetRequest;
+
+/**
+ * Amun_Api_XrdsTest
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://amun.phpsx.org
+ * @category   tests
+ * @version    $Revision: 637 $
+ * @backupStaticAttributes disabled
+ */
+class SitemapTest extends RestTest
+{
+	protected function setUp()
+	{
+		parent::setUp();
+
+		if(!$this->hasService('org.amun-project.sitemap'))
+		{
+			$this->markTestSkipped('Service sitemap not installed');
+		}
+	}
+
+	public function getDataSet()
+	{
+		return $this->createMySQLXMLDataSet('tests/amun.xml');
+	}
+
+	public function getEndpoint()
+	{
+		return $this->config['psx_url'] . '/' . $this->config['psx_dispatch'] . 'api/sitemap';
+	}
+
+	public function getTable()
+	{
+		return DataFactory::getTable('Xrds');
+	}
+
+	public function testGet()
+	{
+		$url      = new Url($this->getEndpoint());
+		$request  = new GetRequest($url);
+		$response = $this->http->request($request);
+
+		$this->assertEquals(200, $response->getCode());
+
+		$xml = simplexml_load_string($response->getBody());
+
+		$this->assertEquals(true, isset($xml->url));
+		$this->assertEquals(true, count($xml->url) > 0);
+
+		foreach($xml->url as $url)
+		{
+			$this->assertEquals(true, isset($url->loc));
+			$this->assertEquals(true, isset($url->lastmod));
+		}
+	}
+}
+
