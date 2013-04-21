58313183fa4de6c18b4be652fef951c1374034bc
Christoph Kappestein <k42b3.x@gmail.com>
2013-04-21 21:55:43 +0200

    fix condition

--- tests/start-webserver.sh
+++ tests/start-webserver.sh
@@ -1,2 +1,2 @@
 version=`php -r "echo version_compare(PHP_VERSION, '5.4.0');"`
-if [$version >= 0]; then echo 'Starting PHP webserver'; php -S localhost:80 -t ../public & fi
+if [ $version -ge 0 ]; then echo 'Starting PHP webserver'; php -S 127.0.0.1:80 -t ../public & fi
