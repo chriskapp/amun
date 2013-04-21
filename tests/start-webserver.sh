34fb24802fefc5bc9231e05c223988243865e868
Christoph Kappestein <k42b3.x@gmail.com>
2013-04-21 21:47:20 +0200

    fix start webserver sh

--- tests/start-webserver.sh
+++ tests/start-webserver.sh
@@ -1,1 +1,2 @@
-if [ '$TRAVIS_PHP_VERSION' = '5.4' ]; then echo 'Starting PHP webserver'; php -S localhost:80 -t ../public & fi
+version=`php -r "echo version_compare(PHP_VERSION, '5.4.0');"`
+if [$version >= 0]; then echo 'Starting PHP webserver'; php -S localhost:80 -t ../public & fi
