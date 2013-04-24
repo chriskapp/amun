3eaae49ca5c0892cd00318173936d0bcc7cb3a65
Christoph Kappestein <k42b3.x@gmail.com>
2013-04-25 00:05:18 +0200

    use port 8000

--- tests/start-webserver.sh
+++ tests/start-webserver.sh
@@ -1,2 +1,2 @@
 version=`php -r "echo version_compare(PHP_VERSION, '5.4.0');"`
-if [ $version -ge 0 ]; then echo 'Starting PHP webserver'; php -S 127.0.0.1:8080 -t .. & fi
+if [ $version -ge 0 ]; then echo 'Starting PHP webserver'; php -S 0.0.0.0:8000 -t .. & fi
