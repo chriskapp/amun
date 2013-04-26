f7aad3bd9e6da0d0478c2ff61418e88d72dd837c
Christoph Kappestein <k42b3.x@gmail.com>
2013-04-26 18:58:07 +0200

    fix base path

--- tests/start-webserver.sh
+++ tests/start-webserver.sh
@@ -1,2 +1,2 @@
 version=`php -r "echo version_compare(PHP_VERSION, '5.4.0');"`
-if [ $version -ge 0 ]; then echo 'Starting PHP webserver'; php -S 0.0.0.0:8000 -t .. & fi
+if [ $version -ge 0 ]; then echo 'Starting PHP webserver'; php -S 0.0.0.0:8000 & fi
