192eb1a2b4cfce9aa18acde34511d4a6b01ab436
Christoph Kappestein <k42b3.x@gmail.com>
2013-04-28 23:54:24 +0200

    add marketplace service

---
+++ service/org.amun-project.marketplace/library/AmunService/Marketplace/Record.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ *  $Id: Api.php 683 2012-06-03 11:52:32Z k42b3.x@googlemail.com $
+ *
+ * amun
+ * A social content managment system based on the psx framework. For
+ * the current version and informations visit <http://amun.phpsx.org>
+ *
+ * Copyright (c) 2010-2012 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of amun. amun is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * amun is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with amun. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace AmunService\Marketplace;
+
+use Amun\DataFactory;
+use Amun\Data\HandlerAbstract;
+use Amun\Data\RecordAbstract;
+use Amun\Exception;
+use Amun\Filter as AmunFilter;
+use Amun\Util;
+use AmunService\Oauth\Filter as OauthFilter;
+use PSX\Data\WriterInterface;
+use PSX\Data\WriterResult;
+use PSX\DateTime;
+use PSX\Filter;
+use PSX\Util\Markdown;
+
+/**
+ * AmunService_Oauth_Record
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://amun.phpsx.org
+ * @category   Amun
+ * @package    Amun_Oauth
+ * @version    $Revision: 683 $
+ */
+class Record extends RecordAbstract
+{
+	protected $_date;
+
+	public function getId()
+	{
+		return $this->_base->getUrn('marketplace', $this->id);
+	}
+
+	public function getDate()
+	{
+		if($this->_date === null)
+		{
+			$this->_date = new DateTime($this->date, $this->_registry['core.default_timezone']);
+		}
+
+		return $this->_date;
+	}
+
+	public function export(WriterResult $result)
+	{
+		switch($result->getType())
+		{
+			case WriterInterface::JSON:
+			case WriterInterface::XML:
+
+				return parent::export($result);
+
+				break;
+
+			case WriterInterface::ATOM:
+
+				$entry = $result->getWriter()->createEntry();
+
+				$entry->setTitle($this->name);
+				$entry->setId('urn:uuid:' . $this->_base->getUUID('marketplace:' . $this->id));
+				$entry->setUpdated($this->getDate());
+				$entry->addAuthor($this->author);
+				$entry->addLink($this->link, 'alternate', 'text/html');
+				$entry->setContent($this->description, 'text');
+
+				return $entry;
+
+				break;
+
+			default:
+
+				throw new Exception('Writer is not supported');
+
+				break;
+		}
+	}
+}
+
+
