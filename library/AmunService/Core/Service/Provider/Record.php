8bf96f4b09f51b6d6bdf13855174b5762c649dda
Christoph Kappestein <k42b3.x@gmail.com>
2013-04-28 23:51:21 +0200

    add core service provider

---
+++ library/AmunService/Core/Service/Provider/Record.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ *  $Id: Option.php 635 2012-05-01 19:46:37Z k42b3.x@googlemail.com $
+ *
+ * amun
+ * A social content managment system based on the psx framework. For
+ * the current version and informations visit <http://amun.phpsx.org>
+ *
+ * Copyright (c) 2010-2012 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of amun. amun is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * amun is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with amun. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace AmunService\Core\Service\Provider;
+
+use Amun\DataFactory;
+use Amun\Data\HandlerAbstract;
+use Amun\Data\RecordAbstract;
+use Amun\Exception;
+use Amun\Filter as AmunFilter;
+use Amun\Util;
+use AmunService\Core\Service\Filter as ServiceFilter;
+use PSX\Data\WriterInterface;
+use PSX\Data\WriterResult;
+use PSX\DateTime;
+use PSX\Filter;
+use PSX\Util\Markdown;
+use PSX\Url;
+
+/**
+ * AmunService_Core_Content_Service_Option
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://amun.phpsx.org
+ * @category   Amun
+ * @package    Amun_Content_Service
+ * @version    $Revision: 635 $
+ */
+class Record extends RecordAbstract
+{
+	public function setId($id)
+	{
+		$id = $this->_validate->apply($id, 'integer', array(new AmunFilter\Id($this->_table)), 'id', 'Id');
+
+		if(!$this->_validate->hasError())
+		{
+			$this->id = $id;
+		}
+		else
+		{
+			throw new Exception($this->_validate->getLastError());
+		}
+	}
+
+	public function setUrl($url)
+	{
+		$url = $this->_validate->apply($url, 'integer', array(new Filter\Length(2, 256), new Filter\Url())), 'url', 'Url');
+
+		if(!$this->_validate->hasError())
+		{
+			$this->url = $url;
+		}
+		else
+		{
+			throw new Exception($this->_validate->getLastError());
+		}
+	}
+}
