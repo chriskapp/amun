f9c6c1f18573bfc4359de750e1222b70875c50cc
Christoph Kappestein <k42b3.x@gmail.com>
2013-05-05 00:09:47 +0200

    add page js test

---
+++ tests/frontend/anonymous/page.js
@@ -0,0 +1,13 @@
+
+testCase('test', {
+
+	view: function(){
+		Assert.object(window.amun.user);
+		Assert.equals(2, window.amun.user.id);
+		Assert.equals('Anonymous', window.amun.user.name);
+
+		Assert.triggerNext();
+	}
+
+});
+
