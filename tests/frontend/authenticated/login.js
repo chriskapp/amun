8b53012d63e4f72b6a385d356114a5249a9c1b9f
Christoph Kappestein <k42b3.x@gmail.com>
2013-06-22 15:54:42 +0200

    add login test

---
+++ tests/frontend/authenticated/login.js
@@ -0,0 +1,13 @@
+
+testCase('login', {
+
+	checkUser: function(){
+		Assert.object(window.amun.user);
+		Assert.equals(1, window.amun.user.id);
+		Assert.equals('test', window.amun.user.name);
+
+		Assert.triggerNext();
+	}
+
+});
+
